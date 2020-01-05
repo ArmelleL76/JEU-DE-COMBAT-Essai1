5c8d703717e8b0b50dd5e0ec56b55333762fcdc1
Armelle <armelle.lecerf@orange.fr>
2020-01-05 17:43:21 +0100

    modification de la class chest

--- JEU DE COMBAT-Essai1/Chest.swift
+++ JEU DE COMBAT-Essai1/Chest.swift
@@ -8,31 +8,25 @@
 
 import Foundation
 class Chest{
-//If a chest appears, the random arm will have  a random strenght
-var randomStrenght : Int = 10
-
-//Initialisation de la propriété
-    init(randomStrenght : Int){
-self.randomStrenght = randomStrenght
-}
-//fonction pour apparition aléatoire du coffre
-  func appear() {
+    var randomStrenght : Int
+    init(randomStrenght : Int)
+    {self.randomStrenght = randomStrenght}
+    func appear()->Bool{
         let random = Int.random(in: 1..<7)
-        if random % 2 == 0
-        { print ("A chest has appeared \n")
-         print("The chest contains a random arm of strenght : \(self.armStrenght()) \n")
-       
-    }
+        if random % 2 == 0 {
+            print("A chest has appeared\n")
+        return true
         }
-    
-  // fonction calculant les dégats aléatoires de l'arme
-    func armStrenght() -> Int {
-        
-     let randomInteger = Int.random(in: 8..<21)
-       self.randomStrenght = randomInteger
-     return randomStrenght
+        else {return false}
     }
-   
-    
-    
+    func armStrenght()->Int{
+        let randomInteger = Int.random(in: 8..<21)
+        self.randomStrenght = randomInteger
+        
+        
+        return randomInteger
+    }
 }
+
+    
+
